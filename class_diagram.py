eefb411f061b4ca872663a11429cc171227a1038
sherinedevi <48640817+sherinedevi@users.noreply.github.com>
2020-11-06 12:04:41 +0700

    fix code

--- class_diagram.py
+++ class_diagram.py
@@ -13,27 +13,39 @@ class User:
         self._email = email
         self._userRole = userRole
         self.message = None
+        
     def login(self, username,password):
-        # check if credentials is 
-        return True
+        if (self.username == username) and (self._password == password):
+            return True
+        else:
+            return False
 
     def verifyAccount(self):
-        return True
+        if (self.username != None) and (self._password != None):
+            return True
+        else:
+            return False
 
     def forgetPassword(self):
-        return True
+        return self._password
 
-    def changePassword(self):
+    def changePassword(self, new_pw):
+        self._password = new_pw
         return True
 
 class Admin(User):
     def verifyUser(self):
-        return True
+        if (self.username != None) and (self._password != None):
+            return True
+        else:
+            return False
 
     def solveThreadProblem(self):
+        print("Thread solved!")
         return True
     
     def verifyPayment(self):
+        print("Payment verified!")
         return True
 
 class Siswa(User):
@@ -50,13 +62,10 @@ class Siswa(User):
 
     def bookTutor(self):
         return True
-    
-    def checkOut(self):
-        return True
 
     def sendMessage(self):
-        message = chatMessage('test','test','test','test')
-        self.message.sendMessage(message)    
+        message = chatMessage('test','test','test')
+        self.message.sendMessage('test')    
 
 class Tutor(User):
     _tutorId = None
@@ -76,27 +85,28 @@ class Tutor(User):
         return True
     
     def sendMessage(self):
-        message = chatMessage('test','test','test','test')
-        self.message.sendMessage(message)
+        message = chatMessage('test','test','test')
+        self.message.sendMessage('test')
 
 
 class chatMessage(User):
     _senderName = None
     _receiveName = None
     _datetime = None
-    _chatHistory = None
+    _chatHistory = []
 
-    def __init__(self,senderName,receiveName,datetime,chatHistory):
+    def __init__(self,senderName,receiveName,datetime):
         self._senderName = senderName
         self._receiveName = receiveName
         self._datetime = datetime
-        self._chatHistory = chatHistory
-
+        self._chatHistory = []
+        
     def seeMessage(self):
-        return True
+        return self._chatHistory
     
-    def sendMessage(self):
-        return self.message
+    def sendMessage(self, message):
+        self._chatHistory.append(message)
+        return self._chatHistory
 
 class tutoringSession:
     _sessionId = None
@@ -105,29 +115,30 @@ class tutoringSession:
     _tutoringPeriod = None
     _paymentStatus = None
 
-    def __init__(self,sessionId,mentorName,siswaName,tutoringPeriod,paymentStatus):
+    def __init__(self,sessionId,mentorName,siswaName):
         self._sessionId = sessionId
         self._mentorName = mentorName
         self._siswaName = siswaName
-        self._tutoringPeriod = tutoringPeriod
-        self._paymentStatus = paymentStatus
+        self._tutoringPeriod = None
+        self._paymentStatus = False
 
     def bookSchedule(self, date):
-        return True
-    
-    def tutoringSession(self):
+        self._tutoringPeriod = date
         return True
 
 class Payment(tutoringSession):
     _mentorFee = None
-    _payerName = None
     _paymentId = None
 
-    def checkOut(self):
+    def checkOut(self, paymentID, fee):
+        self._mentorFee = fee
+        self._paymentId = paymentID
         return True
 
     def verifiedBilling(self):
-        return True
+        if (self._paymentId != None):
+            self._paymentStatus = True
+        return self._paymentStatus
 
 # nunggu relasi dari tutor ama siswa
 class Thread:
@@ -135,6 +146,7 @@ class Thread:
     threadTag = None
     threadHeader = None
     senderName = None
+    replies = []
     
     def createThread(self,threadId,threadTag,threadHeader,senderName):
         self._threadId = threadId
@@ -142,22 +154,30 @@ class Thread:
         self.threadHeader = threadHeader
         self.senderName = senderName
     
-    def replyThread(self):
+    def replyThread(self, reply):
+        self.replies.append(reply)
         return True
 
     def removeThread(self):
+        self._threadId = None
         return True
 
 class Post:
     _threadId = None
     text = None
 
-    def writePost(self):
+    def writePost(self, text):
+        self.text = text
         return True
 
     def seePost(self):
-        return True
+        return self.text
     
     def deletePost(self):
+        self.text = None
         return True
-        
+    
+    
+    
+class __main___:
+    print("main")
